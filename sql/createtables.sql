3a2557b42b65b6c4fda51946aa0bcfabf17757c1
btan0392 <bxtan@asu.edu>
2015-02-21 15:14:15 -0700

    Update createtables.sql

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -26,14 +26,22 @@ Create Table Student (
 
 );
 
-Create Table listing (
+Create Table registration (
+	RegistrNum	int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	StudentNum	int UNSIGNED NOT NULL,
+	ListingNum	int UNSIGNED NOT NULL,
+	RegistrDate	date NOT NULL,
+	FOREIGN KEY (StudentNum) REFERENCES Student(StudentNum),
+	FOREIGN KEY (ListingNum) REFERENCES Listing(ListingNum)
+);
+
+Create Table Listing (
 	ListingNum 	int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	StaffNum 	int UNSIGNED NOT NULL,
-	StudentNum	int UNSIGNED NOT NULL,
+	RegistrNum	int UNSIGNED NOT NULL,
 	headline 	varchar(255) NOT NULL,
 	detailedDescription varchar(255) NOT NULL,
 	category  	varchar(100) NOT NULL,
-	FOREIGN KEY (StaffNum) REFERENCES Staff(StaffNum),
-	FOREIGN KEY (StudentNum) REFERENCES Student(StudentNum)
-	
+	FOREIGN KEY (StaffNum) REFERENCES Staff(StaffNum)
 );
+
