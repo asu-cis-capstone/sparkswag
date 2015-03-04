80376dc5fd81663a0000f80b3dd5b683c685c8cd
walter harvey <wharvery@asu.edu>
2015-03-03 21:36:20 -0700

    Updated controller.

--- pivot/controller/Controller.php
+++ pivot/controller/Controller.php
@@ -15,15 +15,23 @@
 		{
 			if (isset($_GET['opportunity']))
 			{
+				//View will have access to any $variables declared.
+				$pageTitle = 'Create Research Opportunity';
 				$this->model->getAddOpportunity();
 				require 'view/addopportunity.php';
 				
 			}elseif(isset($_GET['user'])){
+				$pageTitle = 'Create User Account';
 				$this->model->getCreateUser();
 				require 'view/createuser.php';
+			}elseif(isset($_GET['???'])){
+			
+			}elseif(isset($_GET['???'])){
+			
 			}
 			else
 			{
+				//Nothing matched.  Defaulting to homepage.
 				$this->model->getHomePage();
 				require 'view/homepage.php';
 			}
