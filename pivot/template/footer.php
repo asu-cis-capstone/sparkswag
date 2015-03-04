02c7c5e639929ed5367efce8b5fe9fc39fd6326d
walter harvey <wharvery@asu.edu>
2015-03-04 10:52:16 -0700

    Updated homepage view.

--- pivot/template/footer.php
+++ pivot/template/footer.php
@@ -1,2 +1,3 @@
 <div id="footer">
+	<p>© 2014 Spark Open Research LLC, all rights reserved</p>
 </div>
--- pivot/view/homepage.php
+++ pivot/view/homepage.php
@@ -5,7 +5,12 @@ require_once("template/navigationbar.php");
 
 
 <div id="contents">
-	<?php echo "homepage!";?>
+	<img src="images/sparklogo.png" alt="Spark Open Research Logo">
+	<h2>discover • connect • learn</h2>
+	<p>Spark allows any student, anywhere to participate in cutting-edge research.
+		We are seeking passionate students and inspiring researchers to join the Spark community
+		to collaborate on pressing challenges in science, technology, engineering and math.</p>
+	
 </div>
 
 
