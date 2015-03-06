ea84d3ecc9aaeb48dc3f0de292a4c94022e42a11
btan0392 <bxtan@asu.edu>
2015-03-05 22:09:44 -0700

    Reedit

--- sql/adduserstoredproc.sql
+++ sql/adduserstoredproc.sql
@@ -1,37 +1,40 @@
-Create Procedure Add_User
-````@fname varchar(32),
-	@lname varchar(32),
-	@mname varchar(32),
-	@userName varchar(20),
-	@address varchar(50),
-	@city varchar(32),
-	@state varchar(15),
-	@zip decimal(10,0),
-	@institution varchar(32),
-	@fieldOfStudy varchar(32),
-	@email varchar(100),
-	@DOB	date,
-	@photo blob,
-	@role varchar(10),
-	@hashedpass varchar(100),
-	@department varchar(32),
-	@staffCV	blob,
-	@title		varchar(32),
-	@gpa		decimal(3,2),
-	@gradeLevel	varchar(10),
-	@major		varchar(32),
-	@studentResm	blob
-As
-	Insert Into User(fname, lname, mname, address, city, state, zip, institution, fieldOfStudy, email, DOB, photo, role, username, hashedpass)
-		Values (@fname, @lname, @mname, @address, @city, @state, @zip, @institution, @fieldOfStudy, @email, @DOB, @photo, @role, @userName, @hashedpass)
-IF (@role = 'Researcher') /*Insert into staff if role is researcher*/
-Begin
-	Insert Into Staff
-		Values((Select UserNum From User Where email = @email), @department, @staffCV, @title)
-End
+Create Procedure Add_User(
+    IN firstname varchar(32),
+	IN lastname varchar(32),
+	IN midname varchar(32),
+	IN userN varchar(20),
+	IN addr varchar(50),
+	IN locCity varchar(32),
+	IN locState varchar(15),
+	IN locZip decimal(10,0),
+	IN institut varchar(32),
+	IN FOS varchar(32),
+	IN emailaddress varchar(100),
+	IN dateOfBirth	date,
+	IN pic blob,
+	IN type varchar(10),
+	IN pass varchar(100),
+	IN staffDepartment varchar(32),
+	IN CV	blob,
+	IN staffTitle		varchar(32),
+	IN gradeAverage		decimal(3,2),
+	IN level	varchar(10),
+	IN study		varchar(32),
+	IN studentResume	blob
+)
 
-Else If(@role = 'Student') /*Insert into student if role is student*/
 Begin
-	Insert Into Student
-		Values((Select UserNum From User Where email = @email), @gpa, @gradeLevel, @major, @studentResm)
-End
+Insert Into User(fname, lname, mname, address, city, state, zip, institution, fieldOfStudy, email, DOB, photo, role, username, hashedpass)
+Values (firstname, lastname, midname, addr, locCity, locState, locZip, institut, FOS, emailaddress, dateOfBirth, pic, type, userN, pass);
+IF (type = 'Researcher') /*Insert into staff if role is researcher*/
+Then Begin
+Insert Into Staff (UserNum, department, staffCV, title)
+Select UserNum From User, staffDepartment, CV, staffTitle Where email = emailaddress;
+End;
+Else If(type = 'Student') /*Insert into student if role is student*/
+Then Begin
+Insert Into Student
+Select UserNum From User, gradeAverage, level, study, studentResume Where email = emailaddress;
+End;
+End If;
+END$$
