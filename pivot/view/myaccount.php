2b20af1e7be0b788eb51229bc26b020bd0bcb4c4
user <walt.harv@gmail.com>
2015-04-19 21:35:37 -0700

    4/19 update

--- pivot/view/myaccount.php
+++ pivot/view/myaccount.php
@@ -10,84 +10,84 @@ require_once("template/navigationbar.php");
 	<?php //d($_SESSION);?>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>User ID: <?php echo $_SESSION['userInfo']['UserNum']; ?>	
 			</p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>User Name: <?php echo $_SESSION['userInfo']['username']; ?>	
 			</p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>First Name: <?php echo $_SESSION['userInfo']['fname']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('fname', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Last Name: <?php echo $_SESSION['userInfo']['lname']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('lname', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Address: <?php echo $_SESSION['userInfo']['address']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('address', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>City: <?php echo $_SESSION['userInfo']['city']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('city', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>State: <?php echo $_SESSION['userInfo']['state']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('state', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Zip code: <?php echo $_SESSION['userInfo']['zip']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('zip', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Institution: <?php echo $_SESSION['userInfo']['institution']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('institution', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Field of Study: <?php echo $_SESSION['userInfo']['fieldOfStudy']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('fieldOfStudy', this, 'text');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>email: <?php echo $_SESSION['userInfo']['email']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('email', this, 'email');"></i></p>
 		</div>
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Date of Birth: <?php echo $_SESSION['userInfo']['DOB']; ?>	
 			<i class="fa fa-pencil-square-o" onClick="ShowEditForm('DOB', this, 'date');"></i></p>
 		</div>
@@ -95,7 +95,7 @@ require_once("template/navigationbar.php");
 	
 	
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-8">
 			<p>Role: <?php echo $_SESSION['userInfo']['role']; ?></p>
 		</div>
 	</div>
